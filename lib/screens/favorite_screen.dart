091116f01d0043930fab18b2a545d3fad28bd6a8
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-06 20:54:13 +0530

    style : text style changed

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:super_extensions/super_extensions.dart';
 import 'package:ui_challenge/providers/providers.dart';
 
+import '../constants/text style/text_style.dart';
 import '../ui components/display_card.dart';
 
 class FavoriteScreen extends ConsumerWidget {
@@ -12,13 +13,16 @@ class FavoriteScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: ListView(
-        children: [
-          ref.watch(likeController).isEmpty
-              ? const Center(
-                  child: Text("No Favorite Properties!"),
-                )
-              : ListView.builder(
+      child: ref.watch(likeController).isEmpty
+          ? Center(
+              child: Text(
+                "You have liked no Properties!",
+                style: UiTextStyle.title.medium,
+              ),
+            )
+          : Column(
+              children: [
+                ListView.builder(
                   itemCount: ref.watch(likeController).length,
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
@@ -28,8 +32,8 @@ class FavoriteScreen extends ConsumerWidget {
                     );
                   },
                 )
-        ],
-      ),
+              ],
+            ),
     );
   }
 }
