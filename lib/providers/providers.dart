54da7a121eb7cd62799ecbe9096db55fd170bacc
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-06 17:26:26 +0530

    feat : search controller to collect search data

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -3,3 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final bottomNavIndex = StateProvider<int>((ref) => 0);
 
 final heartPressed = StateProvider<bool>((ref) => false);
+
+final searchController = StateProvider<String>((ref) => "");
