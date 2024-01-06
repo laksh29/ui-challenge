f91a416d8bc096bae17d9775d93ab32114ae0b8f
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-06 17:27:24 +0530

    refactor : search bar redirect to search screen

--- lib/ui components/search_bar.dart
+++ lib/ui components/search_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ui_challenge/constants/text%20style/text_style.dart';
 
 import '../constants/color_constants.dart';
+import '../screens/search_screen.dart';
 
 class Searchbar extends StatelessWidget {
   const Searchbar({
@@ -14,7 +15,11 @@ class Searchbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SearchScreen(),
+          )),
       child: Container(
         width: screenWidth,
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
