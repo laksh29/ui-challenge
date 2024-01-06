5ea3b328ebf5b046a483c6a13cb8372c39932d2f
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-06 20:01:33 +0530

    feat : new provider created for favorite page

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ui_challenge/data/popular_properties.dart';
 
 final bottomNavIndex = StateProvider<int>((ref) => 0);
 
-final heartPressed = StateProvider<bool>((ref) => false);
+final heartPressed = StateProvider<List<bool>>(
+    (ref) => List.generate(popularProperties.length, (index) => false));
 
 final searchController = StateProvider<String>((ref) => "");
+
+final likeController = StateProvider<List<int>>((ref) => []);
